7d2b38dfbf9c55048ce243d8b6c821b778bf3418
Derrick McKee <derrick.mckee@gmail.com>
2017-09-26 14:06:37 -0400

    adding new script

---
+++ start-qemu2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+IMG=images/disk.img
+INIT_RD=images/initramfs-busybox-x86-2.cpio.gz
+USB_IMG=images/disk.usb
+KERNEL=~/code/kernels/ubuntu-build/arch/x86_64/boot/bzImage
+QEMU_PATH=~/code/qemu/build
+QEMU_EXE=x86_64-softmmu/qemu-system-x86_64
+#QEMU_OPTS="-enable-kvm -kernel $KERNEL -hda $IMG -append \"root=/dev/hda single\""
+#QEMU_OPTS="--enable-kvm -kernel $KERNEL -drive file=$IMG,format=raw -initrd $INIT_RD -m 1024 -append \"init=/init\""
+QEMU_OPTS="--enable-kvm -kernel $KERNEL -initrd $INIT_RD -device piix3-usb-uhci -m 1024"
+#TEL_OPTS="-qmp unix:/tmp/qmp-sock,server --monitor stdio"
+TEL_OPTS="--monitor stdio"
+TRACE_OPTS="-trace events=events"
+
+$QEMU_PATH/$QEMU_EXE $QEMU_OPTS $TEL_OPTS $TRACE_OPTS
